50021815f5c3bf7d97111ef2f58d084ce23fe079
Jose Gallego <108380707+DarkNinja41@users.noreply.github.com>
2025-08-22 11:28:24 -0500

    Clase talleres herramientas 3

---
+++ ProyectoTalleres/TallerPractica1/TallerPractica1/ConductorMetro.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TallerPractica1
+{
+    public abstract class ConductorMetro
+    {
+        public string Nombre { get; set; }
+        public int Turno { get; set; }
+        public int HorasTrabajadas { get; set; }
+
+        protected ConductorMetro(string nombre, int turno, int horasTrabajadas) 
+        { 
+            Nombre = nombre;
+            Turno = turno;
+            HorasTrabajadas = horasTrabajadas;
+        }
+
+        //Método para calcular el salario
+        public virtual double CalcularSalario()
+        {
+            return HorasTrabajadas * 1423500;
+        }
+
+        //Metodo para mostrar los datos
+        public abstract void MostrarDatosConduct();
+    }
+}
---
+++ ProyectoTalleres/TallerPractica1/TallerPractica1/ConductorNocturno.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TallerPractica1
+{
+    public abstract class ConductorNocturno : ConductorMetro
+    {
+        private double RecargoNoct {  get; set; }
+
+        public ConductorNocturno(string nombre, int turno, int horasTrabajadas, double recargoNoct)
+            : base(nombre, turno, horasTrabajadas)
+        {
+            RecargoNoct = recargoNoct;
+        }
+
+        public double SalarioNoct()
+        {
+            return CalcularSalario() * 0.20;
+        }
+
+        public override double CalcularSalario()
+        {
+            return base.CalcularSalario()+;
+        }
+    }
+}
